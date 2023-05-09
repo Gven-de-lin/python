e8ef8a075562fbb08a149e4d4ce6e6b69bac3748
Anfisa Novykova <novik.anfiso4ka@gmail.com>
2023-05-09 19:36:01 +0300

    Exercise 1-4 is done

--- first command.py
+++ first command.py
@@ -1,1 +1,19 @@
-print ("Hello world!")
+#Create a variable called break and assign it a value 5. 
+# See what happens and find out the reason behind the behavior that you see.
+# break=5
+
+#Create two variables. One to store your birth year and another one to store current year. 
+#Now calculate your age using these two variables
+birth_year = 2002
+currently_year = 2023
+years = currently_year - birth_year
+print (years)
+
+#Store your first, middle and last name in three different variables and then print your full name using these variables
+first_name = "Anfisa"
+middle_name = "Novykova"
+last_name = "Valeriivna"
+print (first_name + ' ' + middle_name + ' '+ last_name)
+
+#Answer which of these are invalid variable names: _nation 1record record1 record_one record-one record^one continue
+# Invalid variable is 1record, record-one, record^one.
